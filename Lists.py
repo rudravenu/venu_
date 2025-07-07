4049c6943f17b63d175abcf5dceb1fad64e43553
rudravenu <rudravenu2@gmail.com>
2025-07-07 16:21:59 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,129 @@
+# h=[]
+# for i in range(20):
+#     if i%2==0:
+#         h=h+[i]
+# print(h)  
+
+# h=[i for i in range(30) if i%2==0]
+# print(h)
+
+
+# h=[i*2 for i in range (20) if i%2==0]
+# print(h)
+
+# l=[1,2,3,4,5,6,7,8,9,10]
+# h=[x+2 for x in l if x%2==0]
+# print(h)
+
+# l=[10,11,12,23,45,15]
+# res=1
+# a=[]
+# for x in l:
+#     if x%2==0:
+#         res=res*x
+#         a.append(res)
+# print(a)
+
+# x=['a','Z','A','z']
+# print(ord('a'))
+# print(ord('z'))
+# print(ord('A'))
+# print(ord('Z'))
+
+# x=[69,75,101,107]
+# print(chr(69))
+# print(chr(101))
+# x=[69,75,101,107]
+# for i in x:
+    # print(chr(i))
+
+
+# l=['a','b','c','D']
+# for x in range (len(l)):
+#     print(ord(l[x]))
+
+
+# l=['a','b','c','D']
+# b=[]
+# for i in range(len(l)):
+#     res=ord(l[i])v
+#     b.append(res)
+# print(b)    
+
+# l=['c','v','R']
+# n=[]
+# for i in range(len(l)):
+#     n.append(ord(l[i]))
+# print(n)    
+
+
+# l=['c','d','T']
+# res=[ord(i) for i in l]
+# print(res)
+
+
+# l=['a','D',"f"]
+# res=[]
+# for i in l:
+#     if i in "aeiou":
+#         res+=i
+# print(res)        
+
+
+# l=['c','v','A','a','e']
+# res=[ord(x) for x in l if x in 'aeiou']
+# print(res)
+
+# a=5
+# b=[i*5 for i in range(1,11)]
+# print(b)
+
+# l=['ram','sitha gam','hanuman']
+# res=[x.capitalize() for x in l if len(x)>3]
+# print(res)
+
+# L=[1,2,3,4,5,6,7,8]
+# res=[x for x in L if x%2==0]
+# print(res)
+
+# res=[[i] for i in range(1,10)]
+# print(res)
+
+
+# a=10
+# L=[10,20,40,25,46]
+# res=[x for x in L if x%a==0]
+# print(res)
+
+
+# m=25
+# for x in range(1,m):
+#    print(x)   
+
+# m=10
+# res=[x for x in range(1,m) if x%m==0] #res=0
+# if len(res)==1:  #0: it is a prime number
+#      print('it is prime number')
+# else:
+#       print('it is not prime number')  
+
+# L=[[1,2,3],[4,5,6],[7,8,9]]
+# res=[y for x in L for y in x]
+# print(res)
+
+# a=-1
+# while a>-10:
+#     print(a)
+#     a-=1
+
+# a=-10
+# while a<-1:
+#     print(a)
+#     a+=1
+    
+
+
+
+
+
+    
